be003254b20cfe6cae3a8b9f83c7dff0eb902da2
Code-with-karthik <karthi08030@gmail.com>
2021-11-11 11:31:41 +0530

    removed

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
-from . import views
-
-#URLConf
-urlpatterns = [
-    path('login/', views.login_user),
-    path('home/', views.home)
-]
